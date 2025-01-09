dba05702a14df796a8a0c9cb78d97d94cbe4c812
BugHunter-X <askzenai@gmail.com>
2025-01-09 20:34:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,22 @@
+```javascript
+// pages/about.js
+import { ErrorBoundary } from 'react-error-boundary';
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
+export default function About() {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <div>About Page Content</div>
+    </ErrorBoundary>
+  );
+}
+```
